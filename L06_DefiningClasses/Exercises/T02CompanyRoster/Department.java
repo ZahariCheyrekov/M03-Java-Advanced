53f69184e1986288023e34d1775390dc18d587d9
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-28 20:02:24 +0200

    Update Department.java

--- L06_DefiningClasses/Exercises/T02CompanyRoster/Department.java
+++ L06_DefiningClasses/Exercises/T02CompanyRoster/Department.java
@@ -1,7 +1,9 @@
 package M03_JavaAdvanced.L06_DefiningClasses.Exercises.T02CompanyRoster;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public class Department {
     private String name;
@@ -25,5 +27,12 @@ public class Department {
                 .mapToDouble(Employee::getSalary)
                 .average()
                 .orElse(0.0);
+    }
+
+    public void printDepartment(Department department) {
+        department.getEmployees()
+                .stream()
+                .sorted((e1, e2) -> Double.compare(e2.getSalary(), e1.getSalary()))
+                .forEach(employee -> System.out.println(employee.getIfo()));
     }
 }

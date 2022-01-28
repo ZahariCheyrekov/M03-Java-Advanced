bffff1e4fb8738af922e76b92b651c1c302a4aed
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-28 20:02:51 +0200

    Update Main.java

--- L06_DefiningClasses/Exercises/T02CompanyRoster/Main.java
+++ L06_DefiningClasses/Exercises/T02CompanyRoster/Main.java
@@ -1,6 +1,7 @@
 package M03_JavaAdvanced.L06_DefiningClasses.Exercises.T02CompanyRoster;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,32 +11,31 @@ public class Main {
         int n = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < n; i++) {
             String[] data = scanner.nextLine().split("\\s+");
+
             String name = data[0];
             double salary = Double.parseDouble(data[1]);
             String position = data[2];
             String department = data[3];
+            String email = "n/a";
+            int age = -1;
 
             Employee employee;
 
             if (data.length == 6) {
-                String email = data[4];
-                int age = Integer.parseInt(data[5]);
-                employee = new Employee(name, salary, position, department, email, age);
+                email = data[4];
+                age = Integer.parseInt(data[5]);
 
             } else if (data.length == 5) {
                 String ageOrEmail = data[4];
 
-                try {
-                    int age = Integer.parseInt(ageOrEmail);
-                    employee = new Employee(name, salary, position, department, age);
-
-                } catch (NumberFormatException e) {
-                    employee = new Employee(name, salary, position, department, ageOrEmail);
+                if (Pattern.matches("^[\\d]+$", ageOrEmail)) {
+                    age = Integer.parseInt(ageOrEmail);
+                } else {
+                    email = ageOrEmail;
                 }
-
-            } else {
-                employee = new Employee(name, salary, position, department);
             }
+
+            employee = new Employee(name, salary, position, department, email, age);
 
             if (!departmentMap.containsKey(department)) {
                 departmentMap.put(department, new Department(department));
@@ -43,17 +43,19 @@ public class Main {
             departmentMap.get(department).getEmployees().add(employee);
         }
 
-        Department maxAverageSalaryDepartment = departmentMap
+        Department department = getHighestAverageSalary(departmentMap);
+
+        System.out.println("Highest Average Salary: " + department.getName());
+
+        department.printDepartment(department);
+    }
+
+    private static Department getHighestAverageSalary(Map<String, Department> departmentMap) {
+        return departmentMap
                 .entrySet()
                 .stream()
                 .max(Comparator.comparingDouble(entry -> entry.getValue().getAverageSalary()))
                 .get()
                 .getValue();
-
-        System.out.println("Highest Average Salary: " + maxAverageSalaryDepartment.getName());
-        maxAverageSalaryDepartment.getEmployees()
-                .stream()
-                .sorted((e1, e2) -> Double.compare(e2.getSalary(), e1.getSalary()))
-                .forEach(employee -> System.out.println(employee.getIfo()));
     }
 }

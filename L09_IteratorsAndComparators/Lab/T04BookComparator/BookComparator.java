52cef773bedaa82d2eee0fd4de67d79a8c16d244
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-29 11:24:37 +0200

    Update BookComparator.java

--- L09_IteratorsAndComparators/Lab/T04BookComparator/BookComparator.java
+++ L09_IteratorsAndComparators/Lab/T04BookComparator/BookComparator.java
@@ -3,14 +3,7 @@ package M03_JavaAdvanced.L09_IteratorsAndComparators.Lab.T04BookComparator;
 import java.util.Comparator;
 
 public class BookComparator implements Comparator<Book> {
-    @Override
-    public int compare(Book o1, Book o2) {
-        int bookTitleResult = o1.getTitle().compareTo(o2.getTitle());
-
-        if (bookTitleResult == 0) {
-            return Integer.compare(o1.getYear(), o2.getYear());
-        } else {
-            return bookTitleResult;
-        }
+    public int compare(Book book1, Book book2) {
+        return book1.compareTo(book2);
     }
 }

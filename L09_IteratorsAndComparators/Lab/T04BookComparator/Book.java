845096df68a72b8565b913262099d2821e60b796
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-29 11:24:17 +0200

    Update Book.java

--- L09_IteratorsAndComparators/Lab/T04BookComparator/Book.java
+++ L09_IteratorsAndComparators/Lab/T04BookComparator/Book.java
@@ -4,35 +4,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Book implements Comparable<Book> {
+public class Book implements Comparable<Book>{
     private String title;
     private int year;
     private List<String> authors;
 
     public Book(String title, int year, String... authors) {
-        this.setTitle(title);
-        this.setYear(year);
-        this.setAuthors(authors);
+        this.title = title;
+        this.year = year;
+        setAuthors(authors);
     }
 
-    public String getTitle() {
-        return title;
+    public String toString() {
+        return String.format("%d, %s", year, title);
     }
+
+    public int compareTo(Book book) {
+        if(this.title.equals(book.title))
+            return this.year - book.year;
+        return this.title.compareTo(book.title)
+;    }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public int getYear() {
-        return year;
-    }
-
     public void setYear(int year) {
         this.year = year;
-    }
-
-    public List<String> getAuthors() {
-        return authors;
     }
 
     public void setAuthors(String... authors) {
@@ -43,18 +41,15 @@ public class Book implements Comparable<Book> {
         }
     }
 
-    @Override
-    public String toString() {
-        return String.format("%s (%s)", this.getTitle(), this.getYear());
+    public String getTitle() {
+        return title;
     }
 
-    @Override
-    public int compareTo(Book o) {
-        int bookTitleResult = this.title.compareTo(o.title);
-        if (bookTitleResult == 0) {
-            return Integer.compare(this.year, o.year);
-        } else {
-            return bookTitleResult;
-        }
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
     }
 }

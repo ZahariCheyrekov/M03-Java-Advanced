7a5d0cebee2aadc0501f2957e0557d22ad5c5c64
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-03 16:51:01 +0200

    Update T04Agents.java

--- ExamPreparation/Exam27August2018/T04Agents.java
+++ ExamPreparation/Exam27August2018/T04Agents.java
@@ -25,7 +25,7 @@ public class T04Agents {
             }
             input = scanner.nextLine();
         }
-
+        
         input = scanner.nextLine();
         while (!input.equals("operate")) {
 

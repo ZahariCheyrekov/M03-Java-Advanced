c2e9c55f28aab1c673dfb301781de4eb1e781d78
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-07 17:52:47 +0200

    Update T02MouseAndCheese.java

--- ExamPreparation/Exam23October2021/T02MouseAndCheese.java
+++ ExamPreparation/Exam23October2021/T02MouseAndCheese.java
@@ -3,59 +3,93 @@ package M03_JavaAdvanced.ExamPreparation.Exam23October2021;
 import java.util.Scanner;
 
 public class T02MouseAndCheese {
-    public static char[][] territory;
+    private static char[][] territory;
 
-    public static int mouseRow;
-    public static int mouseCol;
+    private static int mouseRow;
+    private static int mouseCol;
 
-    public static int cheeseEaten = 0;
+    private static int eatenCheese;
 
-    public static boolean disappeared = false;
-    public static boolean enoughCheese = false;
+    private static boolean wentOut;
+
+    private static int row;
+    private static int col;
+
+    private static final int CHEESE_TO_EAT = 5;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = Integer.parseInt(scanner.nextLine());
-
         territory = new char[size][size];
-        readTerritory(scanner);
 
-        String input = scanner.nextLine();
-        while (!input.equals("end")) {
-            String direction = input;
+        fillTerritory(scanner);
+
+        String direction = scanner.nextLine();
+        while (!direction.equals("end")) {
 
             territory[mouseRow][mouseCol] = '-';
             moveMouse(direction);
 
-            if (disappeared) {
+            if (wentOut) {
+                System.out.println("Where is the mouse?");
                 break;
             }
 
-            input = scanner.nextLine();
+            direction = scanner.nextLine();
         }
-
-        if (disappeared) {
-            System.out.println("Where is the mouse?");
-        }
-
-        if (cheeseEaten >= 5) {
-            enoughCheese = true;
-        }
-
-        if (enoughCheese) {
-            System.out.printf("Great job, the mouse is fed %d cheeses!%n", cheeseEaten);
+        if (eatenCheese >= CHEESE_TO_EAT) {
+            System.out.printf("Great job, the mouse is fed %d cheeses!%n",
+                    eatenCheese);
 
         } else {
-            System.out.printf("The mouse couldn't eat the cheeses, she needed %d cheeses more.%n", 5 - cheeseEaten);
+            System.out.printf("The mouse couldn't eat the cheeses, she needed %d cheeses more.%n",
+                    CHEESE_TO_EAT - eatenCheese);
         }
 
-        printMatrix();
+        printTerritory();
     }
 
     private static void moveMouse(String direction) {
-        int row = mouseRow;
-        int col = mouseCol;
+        row = mouseRow;
+        col = mouseCol;
 
+        moveDirection(direction);
+
+        boolean inBounds = checkIfInBounds(row, col);
+        if (inBounds) {
+            char currentCell = territory[row][col];
+
+            switch (currentCell) {
+                case 'c':
+                    eatenCheese++;
+                    break;
+
+                case 'B':
+                    territory[row][col] = '-';
+                    moveDirection(direction);
+
+                    if (territory[row][col] == 'c') {
+                        eatenCheese++;
+                    }
+                    break;
+            }
+
+            mouseRow = row;
+            mouseCol = col;
+
+            territory[mouseRow][mouseCol] = 'M';
+
+        } else {
+            wentOut = true;
+        }
+    }
+
+    private static boolean checkIfInBounds(int row, int col) {
+        return (row >= 0) && (row < territory.length) &&
+                (col >= 0) && (col < territory[row].length);
+    }
+
+    private static void moveDirection(String direction) {
         switch (direction) {
             case "up":
                 row--;
@@ -73,59 +107,9 @@ public class T02MouseAndCheese {
                 col++;
                 break;
         }
-
-        boolean inBounds = checkIfInBounds(row, col);
-        if (inBounds) {
-            char currentCell = territory[row][col];
-
-            switch (currentCell) {
-                case 'c':
-                    cheeseEaten++;
-                    break;
-
-                case 'B':
-                    territory[row][col] = '-';
-
-                    switch (direction) {
-                        case "up":
-                            row--;
-                            break;
-
-                        case "down":
-                            row++;
-                            break;
-
-                        case "left":
-                            col--;
-                            break;
-
-                        case "right":
-                            col++;
-                            break;
-                    }
-
-                    if (territory[row][col] == 'c') {
-                        cheeseEaten++;
-                    }
-                    break;
-            }
-
-            mouseRow = row;
-            mouseCol = col;
-
-            territory[mouseRow][mouseCol] = 'M';
-
-        } else {
-            disappeared = true;
-        }
     }
 
-    private static boolean checkIfInBounds(int row, int col) {
-        return (row >= 0) && (row < territory.length) &&
-                (col >= 0) && (col < territory[row].length);
-    }
-
-    private static void readTerritory(Scanner scanner) {
+    private static void fillTerritory(Scanner scanner) {
         for (int row = 0; row < territory.length; row++) {
             String[] elements = scanner.nextLine().split("");
 
@@ -142,13 +126,16 @@ public class T02MouseAndCheese {
         }
     }
 
-    private static void printMatrix() {
+    private static void printTerritory() {
+        StringBuilder out = new StringBuilder();
+
         for (char[] row : territory) {
             for (char col : row) {
-                System.out.print(col);
+                out.append(col);
             }
-
-            System.out.println();
+            out.append(System.lineSeparator());
         }
+
+        System.out.print(out);
     }
 }

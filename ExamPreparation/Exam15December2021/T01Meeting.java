9d861666511d639eb44e10e929cdda9f07600f4f
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-07 14:50:54 +0200

    Update T01Meeting.java

--- ExamPreparation/Exam15December2021/T01Meeting.java
+++ ExamPreparation/Exam15December2021/T01Meeting.java
@@ -1,85 +1,159 @@
 package M03_JavaAdvanced.ExamPreparation.Exam15December2021;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class T01Meeting {
+public class T02ThroneConquering {
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static char[][] field;
+
+    private static int ParisRow;
+    private static int ParisCol;
+
+    private static int energy;
+
+    private static boolean died;
+    private static boolean reachedHelen;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        energy = Integer.parseInt(scanner.nextLine());
+        int size = Integer.parseInt(scanner.nextLine());
 
-        ArrayDeque<Integer> maleStack = new ArrayDeque<>();
-        fillStack(maleStack, scanner);
+        field = new char[size][];
+        fillField();
 
-        ArrayDeque<Integer> femaleQueue = new ArrayDeque<>();
-        fillQueue(femaleQueue, scanner);
+        findParis();
 
-        int matches = 0;
-        while (!maleStack.isEmpty() && !femaleQueue.isEmpty()) {
-            int male = maleStack.peek();
-            int female = femaleQueue.peek();
+        String input = scanner.nextLine();
+        while (!died && !reachedHelen) {
+            String[] data = input.split("[\\s]+");
 
-            if (female <= 0) {
-                femaleQueue.poll();
+            String direction = data[0];
+            int enemyRow = Integer.parseInt(data[1]);
+            int enemyCol = Integer.parseInt(data[2]);
 
-            } else if (male <= 0) {
-                maleStack.pop();
+            field[enemyRow][enemyCol] = 'S';
+            field[ParisRow][ParisCol] = '-';
 
-            } else if (female % 25 == 0) {
-                femaleQueue.poll();
-                femaleQueue.poll();
+            energy--;
 
-            } else if (male % 25 == 0) {
-                maleStack.pop();
-                maleStack.pop();
+            moveParis(direction);
 
-            } else if (female == male) {
-                matches++;
-                femaleQueue.poll();
-                maleStack.pop();
+            if (reachedHelen) {
+                System.out.printf("Paris has successfully abducted Helen! Energy left: %d%n", energy);
+                break;
+            }
 
-            } else {
-                femaleQueue.poll();
-                int malePoints = male - 2;
-                maleStack.pop();
-                maleStack.push(malePoints);
+            if (died || energy <= 0) {
+                System.out.printf("Paris died at %d;%d.%n", ParisRow, ParisCol);
+                field[ParisRow][ParisCol] = 'X';
+                break;
+            }
+
+            input = scanner.nextLine();
+        }
+
+        printField();
+    }
+
+    private static void moveParis(String direction) {
+        int row = ParisRow;
+        int col = ParisCol;
+
+        switch (direction) {
+            case "up":
+                row--;
+                break;
+
+            case "down":
+                row++;
+                break;
+
+            case "right":
+                col++;
+                break;
+
+            case "left":
+                col--;
+                break;
+        }
+
+        boolean inBounds = checkIfInBounds(row, col);
+
+        if (inBounds) {
+            char currentCell = field[row][col];
+
+            ParisRow = row;
+            ParisCol = col;
+
+            switch (currentCell) {
+                case 'S':
+                    energy -= 2;
+
+                    if (energy <= 0) {
+                        died = true;
+                    }
+                    break;
+
+                case 'H':
+                    reachedHelen = true;
+                    field[ParisRow][ParisCol] = '-';
+                    break;
+
+                case '-':
+                    field[ParisRow][ParisCol] = 'P';
+                    break;
+            }
+
+
+            if (!died && !reachedHelen) {
+                field[ParisRow][ParisCol] = 'P';
             }
         }
+    }
 
-        System.out.println("Matches: " + matches);
+    private static boolean checkIfInBounds(int row, int col) {
+        return (row >= 0) && (row < field.length) &&
+                (col >= 0) && (col < field[row].length);
+    }
 
-        if (maleStack.isEmpty()) {
-            System.out.println("Males left: none");
-
-        } else {
-            System.out.print("Males left: ");
-            System.out.println(String.join(", ",
-                    maleStack.toString()).replaceAll("[\\[\\]]", ""));
-        }
-
-        if (femaleQueue.isEmpty()) {
-            System.out.println("Females left: none");
-
-        } else {
-            System.out.print("Females left: ");
-            System.out.println(String.join(", ",
-                    femaleQueue.toString()).replaceAll("[\\[\\]]", ""));
+    private static void fillField() {
+        for (int row = 0; row < field.length; row++) {
+            field[row] = scanner.nextLine().toCharArray();
         }
     }
 
-    private static void fillStack(ArrayDeque<Integer> maleStack, Scanner scanner) {
-        int[] males = Arrays.stream(scanner.nextLine().split("[\\s]+")).mapToInt(Integer::parseInt).toArray();
+    private static void findParis() {
+        boolean isFound = false;
 
-        for (int m : males) {
-            maleStack.push(m);
+        for (int row = 0; row < field.length; row++) {
+            for (int col = 0; col < field[row].length; col++) {
+                char currentChar = field[row][col];
+
+                if (currentChar == 'P') {
+                    ParisRow = row;
+                    ParisCol = col;
+
+                    isFound = true;
+                }
+            }
+
+            if (isFound) {
+                break;
+            }
         }
     }
 
-    private static void fillQueue(ArrayDeque<Integer> femaleQueue, Scanner scanner) {
-        int[] females = Arrays.stream(scanner.nextLine().split("[\\s]+")).mapToInt(Integer::parseInt).toArray();
+    private static void printField() {
+        StringBuilder out = new StringBuilder();
 
-        for (int f : females) {
-            femaleQueue.offer(f);
+        for (char[] row : field) {
+            for (char col : row) {
+                out.append(col);
+            }
+            out.append(System.lineSeparator());
         }
+
+        System.out.print(out);
     }
 }

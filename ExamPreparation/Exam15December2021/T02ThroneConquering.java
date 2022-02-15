66478efe530d2322ea57ad39bb48a8470ca6e487
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-15 21:18:12 +0200

    Update T02ThroneConquering.java

--- ExamPreparation/Exam15December2021/T02ThroneConquering.java
+++ ExamPreparation/Exam15December2021/T02ThroneConquering.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class T02ThroneConquering {
     private static Scanner scanner = new Scanner(System.in);
-
     private static char[][] field;
 
     private static int ParisRow;

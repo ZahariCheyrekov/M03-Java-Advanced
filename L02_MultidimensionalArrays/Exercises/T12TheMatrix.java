a425d695be40d6aee339ba0c9ad2a8fbf7ba7537
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-16 16:19:35 +0200

    Update T12TheMatrix.java

--- L02_MultidimensionalArrays/Exercises/T12TheMatrix.java
+++ L02_MultidimensionalArrays/Exercises/T12TheMatrix.java
@@ -6,21 +6,22 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class T12TheMatrix {
-    public static char[][] matrix;
+    private static char[][] matrix;
+
+    private final static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int[] dimensions = readData(reader);
+        int[] dimensions = readData();
 
         int rows = dimensions[0];
         int cols = dimensions[1];
 
         matrix = new char[rows][cols];
-        fillMatrix(reader);
+        fillMatrix();
 
         char fillChar = reader.readLine().charAt(0);
 
-        int[] startPosition = readData(reader);
+        int[] startPosition = readData();
 
         int startRow = startPosition[0];
         int startCol = startPosition[1];
@@ -29,12 +30,7 @@ public class T12TheMatrix {
 
         replaceChars(charToReplace, fillChar, startRow, startCol);
 
-        for (char[] row : matrix) {
-            for (char col : row) {
-                System.out.print(col);
-            }
-            System.out.println();
-        }
+        printMatrix();
     }
 
     private static void replaceChars(char charToReplace, char fillChar, int row, int col) {
@@ -56,7 +52,7 @@ public class T12TheMatrix {
                 (matrix[row][col] == charToReplace);
     }
 
-    private static void fillMatrix(BufferedReader reader) throws IOException {
+    private static void fillMatrix() throws IOException {
         for (int row = 0; row < matrix.length; row++) {
             String[] line = reader.readLine().split("\\s+");
             for (int col = 0; col < line.length; col++) {
@@ -65,10 +61,22 @@ public class T12TheMatrix {
         }
     }
 
-    private static int[] readData(BufferedReader reader) throws IOException {
+    private static int[] readData() throws IOException {
         return Arrays.stream(reader.readLine()
-                .split("\\s+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+                        .split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static void printMatrix() {
+        StringBuilder output = new StringBuilder();
+
+        for (char[] row : matrix) {
+            for (char col : row) {
+                output.append(col);
+            }
+            output.append(System.lineSeparator());
+        }
+
+        System.out.print(output);
     }
 }
